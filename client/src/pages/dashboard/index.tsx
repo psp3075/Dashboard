d467ded8384819523b92025eae241216f6edd468
Pavan S Patil <psp3075@gmail.com>
2023-04-19 14:35:04 +0200

    breakdown of rows

--- client/src/pages/dashboard/index.tsx
+++ client/src/pages/dashboard/index.tsx
@@ -1,4 +1,8 @@
-import { Box, useTheme, useMediaQuery } from "@mui/material";
+import DashboardBox from "@/components/DashboardBox";
+import { Box, useMediaQuery } from "@mui/material";
+import Row1 from "./Row1";
+import Row2 from "./Row2";
+import Row3 from "./Row3";
 type Props = {};
 
 const gridTemplateLargeScreens = `
@@ -49,7 +53,6 @@ const gridTemplateSmallScreens = `
 
 const Dashboard = (props: Props) => {
   const isAboveMediumScreens = useMediaQuery("(min-width:1200px)");
-  const { palette } = useTheme();
   return (
     <Box
       width="100%"
@@ -70,16 +73,9 @@ const Dashboard = (props: Props) => {
             }
       }
     >
-      <Box bgcolor="#fff " gridArea="a"></Box>
-      <Box bgcolor="#fff " gridArea="b"></Box>
-      <Box bgcolor="#fff " gridArea="c"></Box>
-      <Box bgcolor="#fff " gridArea="d"></Box>
-      <Box bgcolor="#fff " gridArea="e"></Box>
-      <Box bgcolor="#fff " gridArea="f"></Box>
-      <Box bgcolor="#fff " gridArea="g"></Box>
-      <Box bgcolor="#fff " gridArea="h"></Box>
-      <Box bgcolor="#fff " gridArea="i"></Box>
-      <Box bgcolor="#fff " gridArea="j"></Box>
+      <Row1 />
+      <Row2 />
+      <Row3 />
     </Box>
   );
 };
