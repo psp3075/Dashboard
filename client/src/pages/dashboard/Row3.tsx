ae7ab79c111fa31073051798b107a0af5fbf3e30
Pavan S Patil <psp3075@gmail.com>
2023-05-02 22:19:46 +0200

    added expense breakdown

--- client/src/pages/dashboard/Row3.tsx
+++ client/src/pages/dashboard/Row3.tsx
@@ -1,22 +1,48 @@
-import DashboardBox from "@/components/DashboardBox";
+import { useMemo } from "react";
 import {
   useGetTransactionsQuery,
   useGetProductsQuery,
   useGetKpisQuery,
 } from "@/state/api";
-import { Box, useTheme } from "@mui/material";
+import { Box, Typography, useTheme } from "@mui/material";
 import { DataGrid, GridCellParams } from "@mui/x-data-grid";
+import DashboardBox from "@/components/DashboardBox";
 import BoxHeader from "@/components/BoxHeader";
+import FlexBetween from "@/components/FlexBetween";
+import { PieChart, Pie, Cell } from "recharts";
 
 type Props = {};
 
 const Row3 = (props: Props) => {
   const { palette } = useTheme();
+  const pieColors = [palette.primary[800], palette.primary[500]];
   const { data: transactionData } = useGetTransactionsQuery();
   const { data: productData } = useGetProductsQuery();
   const { data: kpiData } = useGetKpisQuery();
 
   // console.log(transactionData);
+
+  const pieChartData = useMemo(() => {
+    if (kpiData) {
+      const totalExpenses = kpiData[0].totalExpenses;
+      return Object.entries(kpiData[0].expensesByCategory).map(
+        ([key, value]) => {
+          // console.log(totalExpenses - value);
+          // console.log(value);
+          return [
+            {
+              name: key,
+              value,
+            },
+            {
+              name: `${key} of Total`,
+              value: totalExpenses - value,
+            },
+          ];
+        }
+      );
+    }
+  }, [kpiData]);
 
   const productColumns = [
     {
@@ -52,13 +78,13 @@ const Row3 = (props: Props) => {
     {
       field: "amount",
       headerName: "Amount",
-      flex: 0.5,
+      flex: 0.35,
       renderCell: (params: GridCellParams) => `$${params.value}`,
     },
     {
       field: "productIds",
       headerName: "Count",
-      flex: 0.5,
+      flex: 0.1,
       renderCell: (params: GridCellParams) =>
         (params.value as Array<string>).length,
     },
@@ -133,8 +159,54 @@ const Row3 = (props: Props) => {
           />
         </Box>
       </DashboardBox>
-      <DashboardBox gridArea="i"></DashboardBox>
-      <DashboardBox gridArea="j"></DashboardBox>
+      <DashboardBox gridArea="i">
+        <BoxHeader title="Expense Breakdown By Category" sideText="+3%" />
+        <FlexBetween mt="0.5rem" gap="0.5rem" p="0 1rem" textAlign="center">
+          {pieChartData?.map((data, i) => (
+            <Box key={`${data[0].name}-${i}`}>
+              <PieChart width={110} height={100}>
+                <Pie
+                  stroke="none"
+                  data={data}
+                  innerRadius={18}
+                  outerRadius={35}
+                  paddingAngle={2}
+                  dataKey="value"
+                >
+                  {data.map((entry, index) => (
+                    <Cell key={`cell-${index}`} fill={pieColors[index]} />
+                  ))}
+                </Pie>
+              </PieChart>
+              <Typography variant="h5">{data[0].name}</Typography>
+            </Box>
+          ))}
+        </FlexBetween>
+      </DashboardBox>
+      <DashboardBox gridArea="j">
+        <BoxHeader
+          title="Overall Summary and Explanation Data"
+          sideText="+18%"
+        />
+        <Box
+          height="15px"
+          margin="1.25rem 1rem 0.4rem 1rem"
+          bgcolor={palette.primary[800]}
+          borderRadius="1rem"
+        >
+          <Box
+            height="15px"
+            bgcolor={palette.primary[600]}
+            borderRadius="1rem"
+            width="40%"
+          ></Box>
+        </Box>
+        <Typography margin="0 1rem" variant="h6">
+          By focusing on profit margins, pricing strategies, and
+          diversification, companies can improve their financial performance and
+          achieve long-term success.
+        </Typography>
+      </DashboardBox>
     </>
   );
 };
