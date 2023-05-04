7a12da84b3762bd2d11b134c78a0fdca3a731f48
Pavan S Patil <psp3075@gmail.com>
2023-05-04 18:18:07 +0200

    ui change

--- client/src/pages/navbar/index.tsx
+++ client/src/pages/navbar/index.tsx
@@ -10,10 +10,10 @@ const Navbar = (props: Props) => {
   const { palette } = useTheme();
   const [selected, setSelected] = useState("dashboard");
   return (
-    <FlexBetween mb="0.25rem" p="0.5rem 0rem" color={palette.grey[900]}>
+    <FlexBetween mb="0.25rem" p="0.5rem 0rem" color={palette.grey[300]}>
       <FlexBetween gap="0.75rem">
         <PixIcon sx={{ fontSize: "28px" }} />
-        <Typography variant="h4" fontSize="16px" color={palette.grey[900]}>
+        <Typography variant="h4" fontSize="16px">
           DashView
         </Typography>
       </FlexBetween>
@@ -22,7 +22,6 @@ const Navbar = (props: Props) => {
           <Link
             to="/"
             onClick={() => setSelected("dashboard")}
-            className="link"
             style={{
               color: selected === "dashboard" ? "inherit" : palette.grey[800],
               textDecoration: "inherit",
